e4744feabd56ac219ccbe65a02bd90094761dde8
Alexandre Lessa <alexandrelessa73@gmail.com>
2021-12-13 21:49:08 -0300

    estruturando site com js e css

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
- 
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagem/manhã.png'
+        document.body.style.background = '#e0ffff'  
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagem/tarde.png'
+        document.body.style.background = '#bdb76b'
+    } else {
+        img.src = 'imagem/noite.png'
+        document.body.style.background = '#1c1c1c'
+
+    }
 }
